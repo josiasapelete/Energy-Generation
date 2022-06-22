44ac8a0c511c8db2ec5c8fb927f60e61ba290c9b
ABALOGHISLAINE14 <sylvieabalo48gmail.com>
2022-06-22 14:53:48 +0200

    Responsivité

--- space.php
+++ space.php
@@ -13,16 +13,8 @@
      <header>
 
      </header>
-     <main>
-        <section>
-
-        </section>
-        <section>
-           
-        </section>
-
-        <section>
-            
+     <main >
+        
               <section class="contenaire1">
                 <div class="cont1">
                   
@@ -91,31 +83,31 @@
                 </div>
               </section>
               <section class="contenaire3">
-                <h2>Ils croient en nous</h2>
+                <h2 >Ils croient en nous</h2>
                 <div class="container my-3">
                    <div class="row">
-                    <div class="col"><a href=""><img class="w-50" src="image/logo1.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo2.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo3.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo4.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo5.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo6.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo1.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo2.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo3.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo4.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo5.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo6.webp" alt=""></a></div>
                    </div>
                    <div class="row my-5">
-                    <div class="col"><a href=""><img class="w-50" src="image/logo7.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo8.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo9.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo10.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo11.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo12.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo7.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo8.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo9.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo10.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo11.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo12.webp" alt=""></a></div>
                    </div>
                    <div class="row">
-                    <div class="col"><a href=""><img class="w-50" src="image/logo14.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo15.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo16.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo17.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo18.webp" alt=""></a></div>
-                    <div class="col"><a href=""><img class="w-50" src="image/logo19.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo14.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo15.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo16.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo17.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo18.webp" alt=""></a></div>
+                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo19.webp" alt=""></a></div>
                    </div>
                     
                 </div>
@@ -158,12 +150,12 @@
                         <h2>Newsletter</h2>
                         <p>Recevez nos dernières infos, podcats, conseil</p>
                         <p>Nom et prénoms:</p>
-                        <input type="text" name="nom">
+                        <input class="form-control" type="text" name="nom">
                         <P>Email:</P>
-                        <input type="email" name="Email" id="">
+                        <input type="email" class="form-control" name="Email" id="">
                         <option value="">
-                          <input type="checkbox" value="" name="lang" id=""> <label class="label" for="">Je préfère recevoir les informations en français</label> <br>
-                          <input type="checkbox" value="" name="lang" id=""> <label class="label" for="">Je préfère recevoir les informations en anglais</label>
+                          <!-- <input type="checkbox" value="" name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en français</span>  -->
+                          <!-- <input type="checkbox" value="" name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en anglais</span> -->
                         </option>
                         <br>
       
