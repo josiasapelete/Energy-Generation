575645f2e5a5da1fcac17a37eadf775c1ae01d76
josiasapelete <josiasapelete@gmail.com>
2022-06-22 22:25:03 +0200

    commit pour push le navbar include

--- assets/bhs.php
+++ assets/bhs.php
@@ -1,3 +1,4 @@
+<?php require_once('back/connect.php');?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ assets/traitement.php
@@ -0,0 +1,8 @@
+<?php
+require_once("./connect.php");
+    $nom= $_POST['nom'];
+$email= $_POST['Email'];
+var_dump($nom, $email);
+
+
+?>
