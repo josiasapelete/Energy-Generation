a82f09dc745ee945ee200dd452b303568f12938f
ABALOGHISLAINE14 <sylvieabalo48gmail.com>
2022-06-22 15:25:45 +0200

    ajout du footer

--- space.php
+++ space.php
@@ -18,8 +18,10 @@
               <section class="contenaire1">
                 <div class="cont1">
                   
+                  <div class="container">
                   <div class="cont2">
                     <h2 class="container">Energyspace</h2>
+                  </div>
                   </div>
 
                 </div>
@@ -82,108 +84,7 @@
                       </div>
                 </div>
               </section>
-              <section class="contenaire3">
-                <h2 >Ils croient en nous</h2>
-                <div class="container my-3">
-                   <div class="row">
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo1.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo2.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo3.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo4.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo5.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo6.webp" alt=""></a></div>
-                   </div>
-                   <div class="row my-5">
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo7.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo8.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo9.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo10.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo11.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo12.webp" alt=""></a></div>
-                   </div>
-                   <div class="row">
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo14.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo15.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo16.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo17.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo18.webp" alt=""></a></div>
-                    <div class="col-4 col-md-2"><a href=""><img class="w-50" src="image/logo19.webp" alt=""></a></div>
-                   </div>
-                    
-                </div>
-              </section>
-
-              <section class="contenaire4">
-                <h2>Ils parlent de nous</h2>
-                <div class="">
-                  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="" class="d-block w-100" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="" class="d-block w-100" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="" class="d-block w-100" alt="...">
-                      </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Next</span>
-                    </button>
-                  </div>
-
-                </div>
-
-              </section>
-              <section class="">
-                
-                <div class="container-fluid pt-3" id="footer">
-                  <div class="container">
-                    <div class="row gx-md-5">
-                      <div class=" col-12 mt-3 mt-md-0 col-md-6">
-                        <h2>Newsletter</h2>
-                        <p>Recevez nos dernières infos, podcats, conseil</p>
-                        <p>Nom et prénoms:</p>
-                        <input class="form-control" type="text" name="nom">
-                        <P>Email:</P>
-                        <input type="email" class="form-control" name="Email" id="">
-                        <option value="">
-                          <!-- <input type="checkbox" value="" name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en français</span>  -->
-                          <!-- <input type="checkbox" value="" name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en anglais</span> -->
-                        </option>
-                        <br>
-      
-                        <button type="button" class="btn btn-warning">Envoyer</button>
-      
-                      </div>
-      
-                      <div class=" col-12  col-md-6">
-                        <img id="logo" src="image/Logo.png.webp" alt="">
-                        <ul>
-                          <li>Accueil</li> <br>
-                          <li>Business Healthcare school</li> <br>
-                          <li>Blog</li>
-                        </ul>
-      
-                      </div>
-                    </div>
-                  </div>
-                  </div>
               
-
-
-              </section>
-              <section  class="end">
-                <p>2020 - Energie Generation - All rights reserved</p>
-              </section>
-
-        </section>
 
 
 
