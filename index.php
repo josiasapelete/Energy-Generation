fca511ae1efd7a5a2d02e9376383b766863fae5e
akakpodorothee <dakakpoyawa@gmail.com>
2022-06-27 13:18:08 +0000

    ajout des photos et couleurs

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="./assets/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Energie Génération</title>
 
@@ -19,13 +19,13 @@
   <header>
     <nav class="navbar  navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <a class="w-30  navbar-brand" href="#"><img class="logo" src="image/Logo.webp" alt="Logo">
+            <a class="w-30  navbar-brand" href="#"><img class="logo" src="./assets/image/Logo.webp" alt="Logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 offset-md-6 mb-lg-auto ">
+              <ul class="navbar-nav me-auto mb-2 offset-md-6 mb-lg-auto">
                 <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
                 </li>
@@ -40,7 +40,7 @@
                     Nos programmes
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="assets/bhs.php">Business & HealthCare School</a></li>
+                  <li><a class="dropdown-item" href="assets/bhs.php">Business & HealthCare School</a></li>
                 </ul>
                 </li>
                 <li class="nav-item">
@@ -49,16 +49,16 @@
                 <li class="nav-item">
                 <a class="nav-link" aria-current="page" href="assets/blog.php">Blog</a>
                 </li>
-                
-                
                 <li class="nav-item">
                 <a class="nav-link" aria-current="page" href="#">À propos</a>
                 </li>
-            </ul>
-            <div class="lng">
-                <img src="./image/fr.jpeg" alt="" class="fr">
-                <img src="./image/en.jpeg" alt="" class="en">
-            </div>
+                <li class="nav-item">
+                <img src="./assets/image/fr.jpeg" alt="" class="fr">
+                </li>
+                <li class="nav-item">
+                <img src="./assets/image/en.jpeg" alt="" class="en">
+                </li>
+              </ul>
             </div>
             
         </div>
@@ -76,11 +76,12 @@
 
         <div class="container ">
   
-          <div class="row my-5 my-md-0">
+          <div class="row my-5 my-md-4 align-items-center">
             <div class="col-12 col-md-4">
-                <img src="./image/Logo.webp" class=" w-100"  alt="">
+                <img src="./assets/image/Logo.webp" class=" w-100"  alt="">
             </div>
   
+            <div class="col-12 col-md-8">
             <p class="text-lg-start p-3">
               Energie Generation est une organisation panafricaine qui encourage les 
               jeunes Africains à relever les défis les plus pressants de leur génération 
@@ -93,6 +94,7 @@
               Mais avant tout, Energie Generation est une plate-forme centrée sur l'humain
               qui offre aux individus des possibilités de libérer et d'exprimer toute leur créativité.
             </p> 
+            </div>
             
             <br>
 
@@ -108,8 +110,8 @@
           <div class="row  ">
               <div class="col-12 col-md-6  ">
                 <div class="row">
-                  <div class="col-12 col-md-4"  ><img class="w-100" src="./image/image6.webp" alt=""></div>
-                  <div class="col-12 col-md-8" >
+                  <div class="col-12 col-md-6"   ><img class="w-100" src="./assets/image/image6.webp" id="img6"alt=""></div>
+                  <div class="col-12 col-md-6" >
                     <h1>VISION</h1>
                     <p class="text-lg-start p-3" >
                       Permettre l'émergence d'une nouvelle génération africaine en mesure de relever le défi de la dignité humaine sur le continent africain et au-delà.
@@ -122,9 +124,9 @@
           
             <div class="col-12 col-md-6  align-items-center p-4 b ">
               <div class="row">
-                <div class="col-12 col-md-4"><img src="./image/image2.webp"  class="img-thumbnail" alt=""></div>
+                <div class="col-12 col-md-6"><img src="./assets/image/image2.webp"  class="img-thumbnail" id="img2" alt=""></div>
 
-                <div class="col-12 col-md-8 text-lg-start " >
+                <div class="col-12 col-md-6 text-lg-start " >
                   <h1>MISSION</h1>
                   <p class="text-lg-start p-3" >
                     Révolutionner l'accès à l'éducation, à l'accompagnement et au financement, en construisant un écosystème entrepreneurial solide où les jeunes africains peuvent prospérer et développer des entreprises hors du commun.
@@ -146,21 +148,21 @@
           </div> <br>
               <div class="container ">
                   <div class="row">
-                      <div class="col-12 col-md-6 "  ><img class="w-100" src="./image/image8.jpg" alt=""></div>
-                      <div class="col-12 col-md-6">
+                      <div class="col-12 col-md-6 " ><img class="w-100" src="./assets/image/image8.jpg" id="img8" alt=""></div>
+                      <div class="col-12 col-md-6 fs-5">
                           <p class="text-lg-start p-3" >
                               Au sein de nos Business Schools, théorie et pratique se rencontrent pour transformer des idées novatrices en entreprises viables. <br> <br>
                               En tant qu'étudiant porteur de projet, vous suivrez une formation diplômante en entrepreneuriat et aurez l’occasion de bénéficier 
                               de l’expérience pratique des professionnels issus d'entreprises leader mondiales de leur secteur. <br> <br>
                               Au sein de nos écoles, les porteurs de projet bénéficient des installations du laboratoire pour 
-                              développer leur prototype, ainsi que d’un accompagnement personnalisé pour structurer
+                              développer leur prototype, ainsi que d’un <span class="colc" >accompagnement personnalisé</span> pour structurer
                               leur projet entrepreneurial.
                           </p>
                       </div>
                   </div>
               </div> <br> <br>
               
-                <div class=" container" ><img class="w-100" src="image/image5.webp" alt=""></div>
+                <div class=" container" ><img class="w-100" src="./assets/image/image5.webp" alt=""></div>
       </section> <br> <br>
       <section>
           <div class="text-center " id="forma" ><h1>La formation entrepreneuriat <br> solaire</h1></div> 
@@ -169,10 +171,10 @@
       <section>
           <div class="container">
               <div class="row">
-                  <div class=" col-12 col-md-6"> <img src="image/image10.jpeg" class=" w-100" alt=""></div>
+                  <div class=" col-12 col-md-6"> <img src="./assets/image/image10.jpeg" class=" w-100" alt=""></div>
                   <div class="col-12 col-md-6">
-                        <p class="m-3">  Parce que le solaire représente une véritable opportunité de création d'emplois pour les jeunes tout en participant au développement socio-économique des zones rurales africaines notamment, Energy Generation a créé un programme de dix mois pour former puis accompagner des entrepreneurs du solaire. <br> <br>
-                            Quel que soit votre niveau d'études actuel, la Formation Entrepreneuriat Solaire vous propose un programme sur mesure pour vous garantir une formation répondant parfaitement à vos besoins et à vos projets d'avenir. <br>
+                        <p class="m-3">  Parce que le solaire représente <span class="colA" >une véritable opportunité de création d'emplois pour les jeunes </span> tout en participant au développement socio-économique des zones rurales africaines notamment, Energy Generation a créé un programme de dix mois pour former puis accompagner des entrepreneurs du solaire. <br> <br>
+                            Quel que soit votre niveau d'études actuel, la Formation Entrepreneuriat Solaire vous propose un <span class="colB" >programme sur mesure</span> pour vous garantir une formation répondant parfaitement à vos besoins et à vos projets d'avenir. <br>
                         </p>
                   </div>
               </div>
@@ -187,9 +189,9 @@
               <div class="container"  >
                   <p class="text-center p-3" >L’Energy Generation Incubator et le Co-working Space accueillent <br>
       
-                      des start-ups prometteuses dans le domaine de l'énergie, mais pas seulement. <br> <br>
-                      Les services d'incubation incluent un coaching personnalisé, avec formation et mentorat, ainsi <br> qu’un soutien dans les activités quotidiennes et les défis rencontrés. <br> <br>
-                      Afin de soutenir le développement d'un écosystème entrepreneurial international, <br> l'incubateur accueillera des projets issus de l’Academy, mais aussi plusieurs projets de milieux <br> et de pays africains différents.
+                      <span class="col1" >des start-ups prometteuses</span> dans le domaine de l'énergie, mais pas seulement. <br> <br>
+                      Les services d'incubation incluent un coaching personnalisé, avec formation et mentorat, ainsi <br> qu’un <span class="col2">soutien dans les activités quotidiennes et les défis rencontrés</span>. <br> <br>
+                      Afin de soutenir <span class="col3" >le développement d'un écosystème entrepreneurial international</span>, <br> l'incubateur accueillera des projets issus de l’Academy, mais aussi plusieurs projets de milieux <br> et de pays africains différents.
                   </p>
               </div>
           </div>
