ffff9978866ecdb9316d112824fabe8e2d57d776
SylvioSLV <imacg1@iMac-de-Energy-2.local>
2022-06-28 12:35:17 +0000

    Modification au niveau de bhs blog connect et footer

--- assets/connect.php
+++ assets/connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname= "eg_bd";
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname= "eg_bd";
 
-$conn= mysqli_connect($host,$username,$password,$dbname);
+    $conn= mysqli_connect($host,$username,$password,$dbname);
 ?>
