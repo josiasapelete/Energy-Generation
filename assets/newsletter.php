6f2f5f48ddbebac9fd91544c5ae72ae0ee9adc4f
josiasapelete <josiasapelete@gmail.com>
2022-06-28 15:16:29 +0200

    ajout du newsletter back

--- assets/newsletter.php
+++ assets/newsletter.php
@@ -11,27 +11,40 @@
 </head>
 
 <body>
-    <form action="newsletter.php" method="post">
+    <form  method="post">
         <h2>Newsletter</h2>
         <p>Recevez nos dernières infos, podcats, conseil</p>
         <p>Nom et prénoms:</p>
         <input class="form-control" type="text" name="nom">
         <P>Email:</P>
         <input type="email" class="form-control" name="Email" id="">
-        <input type="radio" value="1" name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en français</span>
+        <input type="radio" value="1" checked name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en français</span>
         <input type="radio" value="2" name="lang" id=""> <span class="input-group">Je préfère recevoir les informations en anglais</span>
 
         <br>
-
-        <button type="button" name="send" class="btn btn-warning">Envoyer</button>
+        <input  class="btn btn-warning" type="submit" value="send">
+        <!-- <button type="button" name="send" class="btn btn-warning">Envoyer</button> -->
         <?php
-            if(isset($_POST['send'])){
+        // varible pour recuperer les entrées 
+            if(isset($_POST['submit'])){
                 $name = $_POST["nom"];
         $email = $_POST["Email"];
         $lang = filter_input(INPUT_POST, "lang", FILTER_VALIDATE_INT);
-        var_dump($name,$lang);
-        // if (! $lang) {
-        //     die("Vous devez accepter");}
+
+        // requete pour inserer les donné
+        $sql = "INSERT INTO newsletter (nom,email,lang)
+        values (?,?,?) ";
+
+        $stmt= mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt,$sql)){
+            die(mysqli_error($conn));
+        }
+        mysqli_stmt_bind_param($stmt,"ssi",
+        $name,$email,$lang); 
+        mysqli_stmt_execute($stmt);
+        echo "Informations enrégistrées";
+
+
             }
         ?>
     </form>
