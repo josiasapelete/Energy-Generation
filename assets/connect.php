84e7053909ef8f0029568dac56db13eacb2f9592
josiasapelete <josiasapelete@gmail.com>
2022-06-28 16:20:28 +0200

    changement du nom de la base sur le serveur

--- assets/connect.php
+++ assets/connect.php
@@ -1,8 +1,12 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname= "eg_bd";
+   
+   
+    $host = "epiz_32016530_eg_bd";
+    $username = "ftpupload.net";
+    $password = "LHMhTJDpMeu6V
+";
+    $dbname= "epiz_32016530_eg_bd";
 
     $conn= mysqli_connect($host,$username,$password,$dbname);
+
 ?>
